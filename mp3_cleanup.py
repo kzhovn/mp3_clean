5f1b2bd1d20be178de373a99d15ef78151f39e7a
kzhovn <kzhovn@gmail.com>
2020-10-17 14:05:22 -0400

    Updated ft.

--- mp3_cleanup.py
+++ mp3_cleanup.py
@@ -1,4 +1,4 @@
-import eyed3
+import eyed3 #needs latest version - pip install, not apt-get
 import os
 import re
 import string
@@ -14,7 +14,7 @@ for file in os.listdir(file_list):
     mp3_artist = ""
 
     if mp3.tag.artist is not None:
-        mp3_artist = re.sub(r"(?i)(vevo)|(- Topic)|(lyrics)", "", mp3.tag.artist)   #remove clutter
+        mp3_artist = re.sub(r"(?i)(vevo)|(- Topic)|(lyrics)|(music)", "", mp3.tag.artist)   #remove clutter
         mp3_artist = re.sub(r"([A-Z][a-z0-9]+(?=[A-Z]))", r"\1 ", mp3_artist)       #SplitThisNonsense
         mp3_artist = mp3_artist.split(",", 1)[0]                                    #throw away everything after comma
         mp3_artist = string.capwords(mp3_artist.strip())
@@ -27,16 +27,22 @@ for file in os.listdir(file_list):
 
     if mp3.tag.title is not None:
         mp3_artist_stripped = re.sub(r"([\+-]+)", "", mp3_artist)
-        title_re = re.compile(
+        title_re = re.compile( 
                     fr"""((-\s*)?(\s+by\s+)?({re.escape(mp3_artist)}|{re.escape(mp3_artist_stripped)})(\s*-)?)| #remove artist name
-                    ((with\s+)?(on[-\s]screen\s*)?lyric[s]?)|
-                    (vevo)|(music video)|(((\()|(\[)).*video(\)|\]))|
-                    (audio)|(official)|(HD)|(HQ)|\"|\||\\\\|\/\/|\'\'
+                    ((with\s+)?(on[-\s]screen\s*)?lyric[s]?)|   #[with] [on-screen] lyrics"
+                    (((?!^)\[.*\])|(\[.*\](^$)))|   #anything between square brackets that isn't the entire because some people have to be special and name their songs [i]
+                    (vevo)|(music video)|\(.*video.*\)|(audio)|(official)|(HD)|(HQ) #words, anything in parentheses with the word video
+                    |\"|\||\\\\|\/\/|\'\'   #quotes, |, \\, //, ''
                     """ , re.VERBOSE | re.IGNORECASE)
-        mp3_title = re.sub(title_re, "", mp3.tag.title) 
+        mp3_title = re.sub(title_re, "", mp3.tag.title) #remove all the above
 
-        mp3_title = re.sub(r"(?i)(\(\s*\))|(\[\s*\])|(^\s*:)|(^\s*-$)|(-\s*$)|((\(|\[)\s*(music)\s*(\)|\]))", "", mp3_title) #fix messes from above
+        mp3_title = re.sub(r"(?i)(\(\s*\))|(\[\s*\])|(^\s*_)|(_\s*$)|(^\s*:)|(^\s*-)|(-\s*$)|((\(|\[)\s*(music)\s*(\)|\]))|(\\w\s*($|\)))", "", mp3_title) #fix messes from above
+        mp3_title = re.sub("feat\.", "ft\.", mp3_title)
+        mp3_title = re.sub(r"((?<!\()\s*ft\..*)", r" (\1)", mp3_title) #ft. in parentheses
+        mp3_title = re.sub(r"ft\.(?!\s)", "ft. ", mp3_title) #space after ft.
         mp3_title = re.sub(r"\s\s+", " ", mp3_title).strip() #extra whitespace
+        mp3_title = re.sub(r"\(\s", "(", mp3_title) #no extra parenthese spaces (left)
+        mp3_title = re.sub(r"\s\)", ")", mp3_title) #no extra parenthese spaces (right)
 
     if not mp3_title or mp3_title == " ":
         mp3.tag.title = "Unknown"
